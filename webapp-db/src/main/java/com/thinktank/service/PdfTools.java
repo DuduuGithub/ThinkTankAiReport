aeb8c0ca970b0f8107a42da6b90109091d733443
Mogeny <463586503@qq.com>
2024-12-20 16:46:22 +0800

    加入了处理pdf文件查看请求的controller

--- webapp-db/src/main/java/com/thinktank/service/PdfTools.java
+++ webapp-db/src/main/java/com/thinktank/service/PdfTools.java
@@ -20,4 +20,52 @@ public class PdfTools {
         }
         return java.util.Base64.getEncoder().encodeToString(byteArrayOutputStream.toByteArray());
     }
+    
+
+    
+    /*
+     * 用途：当向数据库插入一个报告时，用来提取文字内容并存到数据库中
+     * 参数：pdf文件的输入流
+     * 返回值：一个包含pdf所有文字的字符串
+     */
+    public static String readPdfContent(InputStream inputStream){
+        try {
+            PDDocument document = PDDocument.load(inputStream);// 加载 PDF 文档
+            
+            PDFTextStripper pdfStripper = new PDFTextStripper();// 创建 PDFTextStripper 对象来提取文本
+            String text = pdfStripper.getText(document);// 提取文本
+            
+            // 关闭文档
+            document.close();
+            return text;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    /*
+     * 用途：读取一个pdf的所有文字内容，用处不大
+     * 参数：一个表示pdf路径的字符串
+     * 返回值：一个包含pdf所有文字的字符串
+     */
+    public static String readPdfContent(String pdfFilePath){
+        try {
+            File file = new File(pdfFilePath);
+            PDDocument document = PDDocument.load(file);
+            
+            // 使用PDFTextStripper提取文本
+            PDFTextStripper stripper = new PDFTextStripper();
+            String text = stripper.getText(document);
+            
+            // System.out.println(text);
+            
+            document.close();
+            return text;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
